91ecc8be63d3926377169af0017f7fb0d8dba09e
Andrew Gardner <timebot@gmail.com>
2015-12-22 00:12:51 -0500

    initial commit

---
+++ Assets/GridClass.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GridClass : MonoBehaviour {
+
+	public static int rows;
+	public static int cols;
+	public GameObject occupiedBy;
+	// Use this for initialization
+	void Start () {
+		rows = 25;
+		cols = 25;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/TileScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileScript : MonoBehaviour {
+
+	public bool occupied;
+	public GameObject occupiedBy;
+	
+	void Start() {
+	}
+	
+	void Update() {
+	}
+}
